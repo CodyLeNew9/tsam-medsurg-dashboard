9bcd9a79cf69bc5651bf6ef057115bf5f2b91403
Cody Newman <Cody.Newman9@outlook.com>
2025-03-02 19:44:20 -0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import TSAMDashboard from '../components/TSAMDashboard';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>TSAM Orientation Dashboard</h1>
+      <TSAMDashboard />
+    </div>
+  );
+}
